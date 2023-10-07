0bfae6b47c93f1a5bee5b03fe0ba9a01e56ffd89
RodicN <rodic.nou@gmail.com>
2023-10-07 13:50:22 +0200

    doublon

--- PHP/PHPdeconnexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy(); // Détruire la session existante
-    header("Location: connexion.php"); // Rediriger vers la page de connexion
-    exit; //quitter le script après la redirection
-?>
