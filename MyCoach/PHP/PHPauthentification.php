8dffacf56b7bbb914b47728f628b020b2cd4b3ca
RodicN <rodic.nou@gmail.com>
2023-10-15 12:54:32 +0200

    Commentaires

--- MyCoach/PHP/PHPauthentification.php
+++ MyCoach/PHP/PHPauthentification.php
@@ -3,27 +3,34 @@
 
     require("../PHP/connexionBdd.php");
 
+    // Vérifie si le formulaire de connexion a été soumis
     if(isset($_POST['valider'])){
-        $email = htmlspecialchars($_POST['email']); // htmlspecialchars empêche l'injection de code html
-        $motdepasse = md5($_POST['motdepasse']); // md5 : hachage du mot de passe
+        $email = htmlspecialchars($_POST['email']); // Évite l'injection de code HTML en convertissant les caractères spéciaux
+        $motdepasse = md5($_POST['motdepasse']); // Hash le mot de passe à l'aide de l'algorithme MD5
 
+        // Prépare une requête SQL pour vérifier les informations de connexion
         $reqSQL = $connexion->prepare("SELECT * FROM utilisateurs WHERE email = ? AND motDePasse = ?");
         $reqSQL->execute(array($email, $motdepasse));
 
+        // Vérifie si une ligne correspondante a été trouvée dans la base de données
         if($reqSQL->rowCount() > 0){
             $ligne = $reqSQL->fetch();
+            
+            // Initialise les sessions pour stocker les informations de l'utilisateur
             $_SESSION['email'] = $email;
             $_SESSION['motDePasse'] = $motdepasse;
             $_SESSION['idUtilisateur'] = $ligne['idUtilisateur'];
             $_SESSION['nom'] = $ligne['nom'];
             $_SESSION['prenom'] = $ligne['prenom'];
 
+            // Redirige l'utilisateur vers la page d'accueil
             header("Location: index.php");
-            exit; //quitte le script après la redirection
+            exit; // Quitte le script après la redirection
         } else {
+            // Affiche un message d'erreur si l'email ou le mot de passe sont incorrects
             echo "Votre email ou mot de passe est incorrect.";
             header("Location: connexion.php");
-            exit; //quitte le script après la redirection
+            exit; // Quitte le script après la redirection
         }
     }
 ?>
--- MyCoach/PHP/PHPinscription.php
+++ MyCoach/PHP/PHPinscription.php
@@ -1,34 +1,46 @@
 <?php
+    // Démarrage de la session
     session_start();
 
+    // Connexion à la base de données
     require("../PHP/connexionBdd.php");
 
+    // Vérification si le formulaire d'inscription a été soumis
     if(isset($_POST['valider'])){
-        $nom = htmlspecialchars($_POST['nom']); // htmlspecialchars empêche l'injection de code html
-        $prenom = htmlspecialchars($_POST['prenom']); // htmlspecialchars empêche l'injection de code html
-        $email = htmlspecialchars($_POST['email']); // htmlspecialchars empêche l'injection de code html
-        $motDePasse = md5($_POST['motdepasse']); // md5 : hachage du mot de passe
+        // Récupération des données depuis le formulaire en utilisant htmlspecialchars pour prévenir l'injection de code HTML
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $email = htmlspecialchars($_POST['email']);
+        
+        // Hachage du mot de passe en utilisant md5
+        $motDePasse = md5($_POST['motdepasse']);
 
+        // Préparation d'une requête SQL pour insérer un nouvel utilisateur dans la base de données
         $reqSQL = $connexion->prepare("INSERT INTO utilisateurs (nom, prenom, email, motDePasse) VALUES (?, ?, ?, ?)");
         $reqSQL->execute(array($nom, $prenom, $email, $motDePasse));
 
+        // Préparation d'une requête SQL pour récupérer l'ID de l'utilisateur nouvellement inscrit
         $reqIDUtilisateur = $connexion->prepare("SELECT * FROM utilisateurs WHERE email = ? AND motDePasse = ?");
         $reqIDUtilisateur->execute(array($email, $motDePasse));
 
+        // Vérification si des résultats ont été trouvés
         if($reqIDUtilisateur->rowCount() > 0){
+            // Initialisation de variables de session avec l'email, le mot de passe et l'ID de l'utilisateur
             $_SESSION['email'] = $email;
             $_SESSION['motDePasse'] = $motDePasse;
             
-            // Utiliser fetch pour obtenir les résultats de la requête
+            // Utilisation de fetch pour obtenir les résultats de la requête
             $ligne = $reqIDUtilisateur->fetch();
             $_SESSION['idUtilisateur'] = $ligne['idUtilisateur'];
         }
 
-        // Test affichage de l'ID utilisateur après l'inscription
+        // Test : affichage de l'ID utilisateur après l'inscription
         // echo $_SESSION['idUtilisateur'];
 
-        // Rediriger vers la page de connexion après l'inscription
+        // Redirection vers la page de connexion après l'inscription
         header("Location: connexion.php");
-        exit; //quitter le script après la redirection
+        
+        // Quitter le script après la redirection
+        exit;
     }
 ?>
